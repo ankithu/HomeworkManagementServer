f46c9930dce52ec0ab4c7788e42b73756910644f
Ankith Udupa <ankith.udupa@gmail.com>
2019-07-29 11:11:59 -0400

    added py files

---
+++ RaspberryPiScripts/automationRequestEngine.py
@@ -0,0 +1,39 @@
+#import necesary libs
+import requests
+import json
+import datetime
+import time as t
+
+#master lists for assignment and alarm storage
+alarms = []
+homework = []
+
+#Loop forever
+while True:
+        #make and process a request to the alarms section of the api. Parse all alarms and add it to the list if it's unique
+        alarmR  = requests.get("http://homework-manage.herokuapp.com/api/alarms")
+        alarmsString = alarmR.content
+        alarmObjects = json.loads(alarmsString)
+        #clears the list in case the user removed alarms using web interface
+        alarms *= 0
+        for alarmObject in alarmObjects:
+                name = alarmObject["alarm"]
+                time = alarmObject["time"]
+                item = {"time": time, "message": name}
+                #precautionary for duplicates
+                if item not in alarms:
+                        alarms.append(item)
+        print(alarms)
+        #make and process a request to the assignments section of the api. Parse all assignments and add it to the list if its unique
+        assignR = requests.get("http://homework-manage.herokuapp.com/api/assignments")
+        assignString = assignR.content
+        assignObjects = json.loads(assignString)
+        for assignObject in assignObjects:
+                name = assignObject["assignment"]
+                duedate = assignObject["duedate"]
+                hour = assignObject["hour"]
+                item = {"duedate":duedate,"hour":hour,"name":name}
+                if item not in homework:
+                        homework.append(item)
+        print(homework)
+        t.sleep(10)
